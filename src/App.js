a0a11b4a708c52e6aaf73fde3cc8940d175d55c3
wadman00 <wady.heriberto@gmail.com>
2022-02-12 23:33:39 +0800

    Klok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+    <button type="button" class="btn btn-success">Klok</button>
+    </div>
+    
+  );
+}
+
+export default App;
