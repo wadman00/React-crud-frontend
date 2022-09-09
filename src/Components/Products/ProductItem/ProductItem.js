9b9a3e4066a70eeec63b7b43979b1e2c6cd83bbc
wadman00 <wady.heriberto@gmail.com>
2022-09-09 23:33:32 +0300

    se le agrego la funcion de eliminar productos

--- src/Components/Products/ProductItem/ProductItem.js
+++ src/Components/Products/ProductItem/ProductItem.js
@@ -1,5 +1,21 @@
 import react from "react";
 
+const deleteProduct = async (_id) =>{
+
+    const response = fetch(`${process.env.REACT_APP_SERVER}/products`,{
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({_id: _id})
+    });
+
+    
+
+    if(response.status === 200){
+        alert("El producto ha sido eliminado");
+
+    }
+}
+
 const ProductItem = ({ product, row }) => {
 
     return (        
@@ -7,6 +23,7 @@ const ProductItem = ({ product, row }) => {
                 <th scope="row">{row}</th>
                 <td>{product.nombre}</td>
                 <td>{product.precio}</td>
+                <td><button className="btn btn-danger" onClick={()=>{deleteProduct(product._id)}}>Eliminar</button></td>
             </tr>  
     );
 }
